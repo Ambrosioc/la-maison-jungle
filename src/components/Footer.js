75b5cac3ee17d1dc5745c86a38d41b343420dc10
Ambrosie Cazimira <a.cazimira@gmail.com>
2022-10-02 16:15:41 +0200

    add input on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Footer.css";
 
 function Footer() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInput = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleBlur = () => {
+    if (!inputValue.includes("@")) {
+      alert("Please enter a valid email");
+    }
+  };
+
   return (
     <footer className="lmj-footers">
       <div className="lmj-fotter-elem">
         Pour les passionné·e·s de plantes 🌿🌱🌵
       </div>
       <div className="lmj-fotter-elem">Laissez-nous votre mail :</div>
+      <input
+        placeholder="Entrez votre mail"
+        onChange={handleInput}
+        value={inputValue}
+        onBlur={handleBlur}
+      />
     </footer>
   );
 }
